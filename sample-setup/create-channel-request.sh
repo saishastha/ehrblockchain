b8ccbc30c796f2ab50c4fae3015d4e3c44e2db20
saishastha <36319071+saishastha@users.noreply.github.com>
2021-06-10 19:25:37 +0530

    cmd change

--- sample-setup/create-channel-request.sh
+++ sample-setup/create-channel-request.sh
@@ -8,5 +8,5 @@ set -e
 export CHANNEL_NAME=providerschannel
 export ORDERER_NAME=orderer0.hospital1.example.com
 echo "Create channel request:Start"
-peer channel create -o $ORDERER_NAME:7050 -c $CHANNEL_NAME -f ./channel-artifacts/$CHANNEL_NAME.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/hospital1.example.com/peers/$ORDERER_NAME/msp/tlscacerts/tlsca.hospital1.example.com-cert.pem
+docker exec cli peer channel create -o $ORDERER_NAME:7050 -c $CHANNEL_NAME -f ./channel-artifacts/$CHANNEL_NAME.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/hospital1.example.com/peers/$ORDERER_NAME/msp/tlscacerts/tlsca.hospital1.example.com-cert.pem
 echo "Create channel request:Done"
